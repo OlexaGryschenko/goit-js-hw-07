9a3f3ccc4a7ebf71e386c70f6164941ff8c419d4
WIN-QUG5H71A7UD <baraban777771962@gmail.com>
2026-05-18 13:33:52 +0100

    feat: tasks 4 and 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const {email, password } = e.currentTarget.elements;
+
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (emailValue === '' || passwordValue === '' ){
+        return alert('All form fields must be filled in');
+    }
+    const data = {
+        email: emailValue,
+        password: passwordValue
+    };
+    
+    console.log(data);
+    e.currentTarget.reset();
+});
